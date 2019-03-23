aaec4080a0e08e166c51672878675e877b4c3b1b
Ronac <hrvoje.roncevic@gmail.com>
2019-03-23 23:54:27 +0100

    Spring boot no manifest fixed

--- reinstallApp.bat
+++ reinstallApp.bat
@@ -1,14 +1,20 @@
 REM ------------ 1 ------------------
-call mvn clean install
-
-REM ------------ 2 ------------------
 call docker stop authws_dock
 
-REM ------------ 3 ------------------
+REM ------------ 2 ------------------
 call docker rm authws_dock
 
-REM ------------ 4 ------------------
-call docker build -t authws:latest .
+REM ------------ 3 ------------------
+rem https://stackoverflow.com/questions/19620342/failed-to-load-main-class-manifest-attribute-while-running-java-jar
+call mvn clean package spring-boot:repackage
+
+REM ------------ 4 -----------------
+del D:\Temp\application.log
 
 REM ------------ 5 ------------------
+call docker build -t authws:latest .
+
+PAUSE
+
+REM ------------ 6 ------------------
 call docker run -d --name authws_dock -v D:/Temp:/var/log/Application/ -p 8082:8082 authws:latest
